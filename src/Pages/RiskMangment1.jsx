a01a697f4c25afdb183a947335b0b36feb33c9eb
Rogar-IQ <ms5325747@gmail.com>
2022-02-19 10:20:37 +0200

    Fixed

--- src/Pages/RiskMangment1.jsx
+++ src/Pages/RiskMangment1.jsx
@@ -80,7 +80,7 @@ const RiskMangment = () => {
 				h="100vh"
 				gap="5"
 				flexDirection={{ md: "row", base: "column" }}
-				mt="10"
+				mt={{ md: "100px", base: "0px" }}
 			>
 				<Box
 					boxShadow="dark-lg"
